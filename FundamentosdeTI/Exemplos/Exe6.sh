0c9fa116a4912232ba6c136a163b27f0c75890d7
AdemarANeto <83764632+AdemarANeto@users.noreply.github.com>
2021-06-05 14:58:57 -0300

    Acrescentado o exercício 6.

---
+++ FundamentosdeTI/Exemplos/Exe6.sh
@@ -0,0 +1,17 @@
+# autor: Ademar Neto.
+# descrição: neste exercío 6 deveríamso modificar o exercício 5 para ele somente aceitar valores positivos.
+#!/bin/bash
+if [ $3 -eq 1 ]
+then
+ if test $1 -lt 0
+ then
+  echo "$1 é neagtivo, digite um valor positivo"
+ fi
+i=$1
+ for i in $(seq 0 $1)
+ do
+  echo $i
+ done
+else
+ echo "Digite um único valor positivo"
+fi
