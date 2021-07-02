90039094d11ae41427cabec08b5873c5718f6c6c
AdemarANeto <83764632+AdemarANeto@users.noreply.github.com>
2021-07-02 08:55:45 -0300

    Criado o exercício B.

---
+++ FundamentosdeTI/Exemplos/01-07-2021 ExeB.sh
@@ -0,0 +1,4 @@
+# Autor: Ademar Neto.
+# Descrição: No exercício "B" nós deveriamos fazer um script para imprimir uma lista de frutas com os seus respectivos preços, vale lembrar que esta lista estava em outro arquivo.
+#!/bin/bash
+cut -d" " -f2,3 Frutas.txt
