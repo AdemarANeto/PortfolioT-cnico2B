97f6982551329ae27f022c324a29ff4bdab71ac8
AdemarANeto <83764632+AdemarANeto@users.noreply.github.com>
2021-07-02 08:58:37 -0300

    Criado o exercício C.

---
+++ FundamentosdeTI/Exemplos/01-07-2021 ExeC.sh
@@ -0,0 +1,4 @@
+# Autor: Ademar Neto.
+# Descrição: No exercício "C" nós deveriamos modificar o Exercício "B" fazendo a lista aparecer em ordem alfabética.
+#!/bin/bash
+cut -d" " -f2,3 Frutas.txt | sort
