0d8ed4a62addfd1803ec319558e3c954c350ae0e
AdemarANeto <83764632+AdemarANeto@users.noreply.github.com>
2021-07-02 08:53:14 -0300

    Criado o exercício A.

---
+++ FundamentosdeTI/Exemplos/01-07-2021 ExeA.sh
@@ -0,0 +1,4 @@
+# Autor: Ademar Neto.
+# Descrição: Neste exercício "A" nós deviamos fazer um script para imprimirmos uma lista de frutas com o nome destas que estava dentro de outro arquivo.
+#!/bin/bash
+cut -d" " -f2 Frutas.txt
